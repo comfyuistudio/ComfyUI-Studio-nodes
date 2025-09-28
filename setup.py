353cdf7586ed1630e0ab2ea4d5e4d22d1549cd84
Alex <sanyi0007@gmail.com>
2025-09-28 20:31:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='ComfyUI-Studio-nodes',
-    version='0.1.0',
-    description='A set of aspect ratio tools for ComfyUI: resizing, label generation, and markdown export.',
-    author='Your Name',
+    version='0.1.5',
+    description='Image Size, Aspect Ratio, Image resize, HuggingFace model downloader, Git cloner',
+    author='ComfyUI-Studio',
     packages=find_packages(where="custom_nodes"),
     package_dir={"": "custom_nodes"},
     install_requires=[
